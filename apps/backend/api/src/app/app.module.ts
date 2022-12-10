8026813a5aa07c62368c74b3a87f2b7662b67197
konstantin <kvengerov@gmail.com>
2022-12-11 00:57:50 +0200

    feat(db): add db
    * add @nestjs/typeorm typeorm pg
    * add scripts
    
    Closes #6

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...environment.connection,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
